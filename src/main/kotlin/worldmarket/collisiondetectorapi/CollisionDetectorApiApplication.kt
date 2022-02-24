3e93446bfd5134293b493e5f7c166d0b04cca604
sayheyBongmany <48705652+sayheyBongmany@users.noreply.github.com>
2022-02-25 00:47:58 +0900

    Initial commit

---
+++ src/main/kotlin/worldmarket/collisiondetectorapi/CollisionDetectorApiApplication.kt
@@ -0,0 +1,11 @@
+package worldmarket.collisiondetectorapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollisionDetectorApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CollisionDetectorApiApplication>(*args)
+}
---
+++ src/test/kotlin/worldmarket/collisiondetectorapi/CollisionDetectorApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package worldmarket.collisiondetectorapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CollisionDetectorApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
